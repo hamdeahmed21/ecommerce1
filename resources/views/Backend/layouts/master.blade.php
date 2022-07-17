dee5977c5e4660613d208e7286ea5f74717c70d5
hamde abo khadra <hamdeahmed01221@gmail.com>
2022-07-17 04:18:15 +0200

    ecommerce1

---
+++ resources/views/Backend/layouts/master.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="{{asset('img/logo/logo.png')}}" rel="icon">
+    <title>@yield('title')</title>
+    <link href="{{asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('css/ruang-admin.min.css')}}" rel="stylesheet">
+    @yield('css')
+</head>
+
+<body id="page-top">
+<div id="wrapper">
+   @include('Backend.layouts.sidebar')
+    <div id="content-wrapper" class="d-flex flex-column">
+        <div id="content">
+           @include('Backend.layouts.header')
+
+ @yield('content')
+        </div>
+      @include('Backend.layouts.footer')
+    </div>
+</div>
+
+<!-- Scroll to top -->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
+
+<script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+<script src="{{asset('js/ruang-admin.min.js')}}"></script>
+<script src="{{asset('vendor/chart.js/Chart.min.js')}}"></script>
+<script src="{{asset('js/demo/chart-area-demo.js')}}"></script>
+@yield('js')
+</body>
+
+</html>
