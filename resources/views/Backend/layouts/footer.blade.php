6bce8fc16925721402ca577d879ef30694fa90ad
hamde abo khadra <hamdeahmed01221@gmail.com>
2022-07-20 01:25:04 +0200

    Home

--- resources/views/Backend/layouts/footer.blade.php
+++ resources/views/Backend/layouts/footer.blade.php
@@ -9,3 +9,29 @@
     </div>
 </footer>
 <!-- Footer -->
+<script type="text/javascript">
+    function confirmDelete(){
+        return confirm('Are you sure you want to delete?');
+    }
+</script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#summernote').summernote();
+    });
+</script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#summernote1').summernote();
+    });
+</script>
+
+<!-- Page level custom scripts -->
+<script>
+    $(document).ready(function () {
+        $('#dataTable').DataTable(); // ID From dataTable
+        $('#dataTableHover').DataTable(); // ID From dataTable with Hover
+    });
+</script>
+
+
+
